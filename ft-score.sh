01b3d09edead776410f519d0b2d247b439230735
Phil de Joux <philderbeast@gmail.com>
2020-10-24 20:47:00 -0400

    Score Italy PG open.

--- ft-score.sh
+++ ft-score.sh
@@ -8,7 +8,7 @@ fs-arrival "$COMP"
 fs-effort "$COMP"
 fs-score "$COMP"
 
-ft-extract-input "$COMP" --give-fraction=0.001 --fsandoyer
+ft-extract-input "$COMP" --give-fraction=0.005 --haversines
 ft-task-length "$COMP"
 ft-cross-zone "$COMP"
 ft-tag-zone "$COMP"
